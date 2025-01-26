580090113e1c91f1ce71ab4b2b8277fe861168c7
Arianna Vega <ariannavega2021@gmail.com>
2025-01-26 00:05:51 -0400

    add react-router-dom for routing and wrap App component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import "@mantine/core/styles.css";
 import App from "./App.jsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <MantineProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </MantineProvider>
-      <ReactQueryDevtools />
+      <ReactQueryDevtools/>
     </QueryClientProvider>
   </StrictMode>
 );
