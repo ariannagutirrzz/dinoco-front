aacb006aa57fb9e737f8b7e088a8e06019460c46
Arianna Vega <ariannavega2021@gmail.com>
2025-01-25 22:30:22 -0400

    add PostCSS configuration and integrate Mantine and React Query into the application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
+import "./index.css";
+import "@mantine/core/styles.css";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </StrictMode>
+);
