5eb3b1e44d0a42e7dd9db97fce6afbd461a358df
Arianna Vega <ariannavega2021@gmail.com>
2025-02-27 03:54:21 -0400

    feat: Add BarChartHorizontal component and integrate it into Charts page

--- src/components/Charts/BarChartHorizontal.jsx
+++ src/components/Charts/BarChartHorizontal.jsx
@@ -1,21 +1,26 @@
-import React from "react";
 import { scaleBand, scaleLinear, max } from "d3";
 import { ClientTooltip, TooltipContent, TooltipTrigger } from "./ClientTooltip"; // Ensure this path is correct
 import { useProducts } from "../../hooks/useProducts";
 
-
 const data = [
-  { key: "Luz led c6 9006", value: 10 },
-  { key: "lupitas h4", value: 20 },
-  { key: "Tercer stop", value: 30 },
-  { key: "Led Auxiliar 18w", value: 40 },
-  { key: "Luz led c6 90", value: 80 },
-  { key: "lupitas h2", value: 14 },
- 
+  { key: "Technology", value: 76},
+  { key: "Financials", value: 25.3 },
+  { key: "Energy", value: 23.1 },
+  { key: "Cyclical", value: 19.5 },
+  { key: "Defensive", value: 14.7 },
+  { key: "Utilities", value: 5.8 },
+  { key: "Healthcare", value: 5.3 },
+  { key: "Industrials", value: 4.7 },
+  { key: "Materials", value: 4.4 },
+  { key: "Industrial Goods", value: 1.1 },
+  { key: "Energy Sector", value: 1.0 },
+  { key: "Technology Sector", value: 0.9 },
+  { key: "Healthcare Sector", value: 0.8 },
+  { key: "Consumer Goods", value: 0.7 },
+  { key: "Financial Sector", value: 0.6 },
 ].sort((a, b) => b.value - a.value);
 
-export function BarChartThinHorizontal() {
-
+export function BarChartHorizontal() {
 
   //  We have to create a specific query from supabase to get the data we need for the chart - exmaple: the most saled products, then we will create a custom hook and utilize it here
 
@@ -30,12 +35,11 @@ export function BarChartThinHorizontal() {
   // }))
   // .sort((a, b) => b.value - a.value);
 
-
   // Scales
   const yScale = scaleBand()
     .domain(data.map((d) => d.key))
     .range([0, 100])
-    .padding(0.6);
+    .padding(0.175);
 
   const xScale = scaleLinear()
     .domain([0, max(data.map((d) => d.value)) || 0])
@@ -58,91 +62,85 @@ export function BarChartThinHorizontal() {
         className="absolute inset-0
           z-10
           h-[calc(100%-var(--marginTop)-var(--marginBottom))]
-          translate-y-[var(--marginTop)]
           w-[calc(100%-var(--marginLeft)-var(--marginRight))]
           translate-x-[var(--marginLeft)]
+          translate-y-[var(--marginTop)]
           overflow-visible
-        "
+           " // border border-gray-200 dark:border-gray-500 Added border to the chart area
       >
-        {/* Tooltip for each data point */}
         {data.map((d, index) => {
           const barWidth = xScale(d.value);
           const barHeight = yScale.bandwidth();
-          const hoverColor =
-            barWidth > 50
-            ? "hover:bg-pink-200/40"
-            : barWidth > 25
-            ? "hover:bg-purple-200/40"
-            : barWidth > 10
-            ? "hover:bg-indigo-200/40"
-              : "hover:bg-sky-200/40";
+
           return (
             <ClientTooltip key={index}>
               <TooltipTrigger>
                 <div
                   key={index}
                   style={{
-                    position: "absolute",
                     left: "0",
                     top: `${yScale(d.key)}%`,
-                    width: "100%",
-                    height: `calc(${barHeight}% + 8px)`,
-                    transform: "translateY(-4px)",
+                    width: `${barWidth}%`,
+                    height: `${barHeight}%`,
+                    borderRadius: "0 4px 4px 0",
                   }}
-                  className={`${hoverColor} rounded-md hover:bg-gray-200/50 relative z-10`}
+                  className="absolute bg-purple-300 dark:bg-purple-400"
                 />
               </TooltipTrigger>
               <TooltipContent>
-                <div>{d.key}</div>
-                <div className="text-gray-500 text-sm">{d.value}</div>
+                <div className="flex gap-2.5 items-center">
+                  <div className="w-1 h-8 bg-purple-300 dark:bg-purple-400 rounded-full"></div>
+                  <div>
+                    <div className="text-white">{d.key}</div>
+                    <div className="text-white text-sm/5">{d.value}%</div>
+                  </div>
+                </div>
               </TooltipContent>
             </ClientTooltip>
           );
         })}
 
-        {/* Bars with Rounded Right Corners */}
-        {data.map((d, index) => {
-          const barWidth = xScale(d.value);
-          const barHeight = yScale.bandwidth();
-          const colorMap = {
-            top: "#0098da",
-            mid: "#3698ff",
-            bottom: "#36c2ff",
-            sky: "#0271d8",
-          };
-
-          const barColor =
-            barWidth > 50 ? colorMap.top
-            : barWidth > 25 ? colorMap.bottom
-            : barWidth > 10 ? colorMap.bottom
-            : colorMap.sky;
-          return (
-            <React.Fragment key={index}>
-               <svg
-                style={{
-                  position: "absolute",
-                  left: "0",
-                  top: `${yScale(d.key)}%`,
-                  width: `${barWidth}%`,
-                  height: `${barHeight}%`,
-                }}
+        {/* Grid lines - Less dark */}
+        <svg
+          className="h-full w-full"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+        >
+          {xScale
+            .ticks(8)
+            .map(xScale.tickFormat(8, "d"))
+            .map((active, i) => (
+              <g
+                transform={`translate(${xScale(+active)},0)`}
+                className="text-gray-300/50 dark:text-gray-800/30" // opacity for lighter grid lines
+                key={i}
               >
-                <rect
-                  x="0"
-                  y="0"
-                  width="100%"
-                  height="100%"
-                  rx="8"
-                  ry="8"
-                  fill={
-                    barColor
-                  }
+                <line
+                  y1={0}
+                  y2={100}
+                  stroke="currentColor"
+                  strokeDasharray="6,5"
+                  strokeWidth={0.5}
+                  vectorEffect="non-scaling-stroke"
                 />
-              </svg>
-            </React.Fragment>
-              );
-            })}
+              </g>
+            ))}
+        </svg>
+
+        {/* X Axis (Values) */}
+        {xScale.ticks(4).map((value, i) => (
+          <div
+            key={i}
+            style={{
+              left: `${xScale(value)}%`,
+              top: "100%",
+            }}
+            className="absolute text-xs -translate-x-1/2 tabular-nums text-gray-400"
+          >
+            {value}
           </div>
+        ))}
+      </div>
 
       {/* Y Axis (Letters) */}
       <div
@@ -156,10 +154,10 @@ export function BarChartThinHorizontal() {
           <span
             key={i}
             style={{
-              left: "0",
+              left: "-8px",
               top: `${yScale(entry.key) + yScale.bandwidth() / 2}%`,
             }}
-            className="absolute text-xs text-black -translate-y-1/2 w-full text-right pr-2"
+            className="absolute text-xs text-black -translate-y-1/2 w-full text-right"
           >
             {entry.key}
           </span>
