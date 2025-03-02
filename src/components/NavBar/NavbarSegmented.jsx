6e02e8900e03c1e8b65d5cd6766d8c08f54366e5
Arianna Vega <ariannavega2021@gmail.com>
2025-03-02 00:41:57 -0400

    fix: Remove unused icon imports from NavbarSegmented component

--- src/components/NavBar/NavbarSegmented.jsx
+++ src/components/NavBar/NavbarSegmented.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import {
-  Icon2fa,
-  IconFileAnalytics,
   IconFingerprint,
   IconCreditCardPay,
   IconLicense,
